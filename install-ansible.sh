23cf3b894ebf992c5208652ecb5b7c6dd045305e
Andres Caicedo <andrezz1997@gmail.com>
2023-03-03 21:28:22 +0100

    Delete install-ansible.sh

--- install-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script automatic setup Ansible repository and install last version.
-
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
-sudo apt install ansible -y
